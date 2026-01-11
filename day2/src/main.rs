d4141ea840a4ffe11ef66bca4977ead077e7c0ad
Felix <felix.camrath@gmail.com>
2026-01-11 16:30:43 +0100

    feat(day2): added second task to main

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,14 +7,26 @@ use crate::{logic::{Game, Hand}, parsing::Parse};
 
 fn main() {
     let puzzle_input = fs::read_to_string("input").expect("expect file to be read");
-    let max_hand = Hand::new(12, 13, 14);
-
     let games: Vec<Game> = puzzle_input.lines()
         .map(|line| Game::parse(line))
         .collect::<Option<Vec<Game>>>()
         .unwrap();
+
+    task1(&games);
+    task2(&games);
+}
+
+fn task1(games: &Vec<Game>) {
+    let max_hand = Hand::new(12, 13, 14);
     let games = games.iter()
         .filter(|g| g.is_possible(&max_hand));
     let sum: i32 = games.map(|g| g.get_id()).sum();
     println!("Sum of possible game id's: {sum}");
+}
+
+fn task2(games: &Vec<Game>) {
+    let power: i32 = games.iter()
+        .map(|g| g.get_min_hand().get_power())
+        .sum();
+    println!("Sum of minimal game powers: {power}")
 }
